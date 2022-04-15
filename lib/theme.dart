0f7fe4d655be881189bba033b4061163abef67fe
acepnurmansidik <acepnurmansidik@gmail.com>
2022-04-15 16:56:47 +0700

    add const at theme(Color)

--- lib/theme.dart
+++ lib/theme.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-Color kPrimaryColor = Color(0xFF5C40CC);
-Color kBlackColor = Color(0xFF1F1449);
-Color kWhiteColor = Color(0xFFFFFFFF);
-Color kGreyColor = Color(0xFF9698A9);
-Color kGreenColor = Color(0xFF0EC3AE);
-Color kRedColor = Color(0xFFEB70A5);
-Color kBackgroundColor = Color(0xFFFAFAFA);
-Color kInactiveColor = Color(0xFFDBD7EC);
+Color kPrimaryColor = const Color(0xFF5C40CC);
+Color kBlackColor = const Color(0xFF1F1449);
+Color kWhiteColor = const Color(0xFFFFFFFF);
+Color kGreyColor = const Color(0xFF9698A9);
+Color kGreenColor = const Color(0xFF0EC3AE);
+Color kRedColor = const Color(0xFFEB70A5);
+Color kBackgroundColor = const Color(0xFFFAFAFA);
+Color kInactiveColor = const Color(0xFFDBD7EC);
 
 // TextStyle ===========================================
 TextStyle blackStyle = GoogleFonts.poppins(
