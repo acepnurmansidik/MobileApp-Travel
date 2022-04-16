9bd512d5b9aef216aea3ab26481fdb4808893637
acepnurmansidik <acepnurmansidik@gmail.com>
2022-04-16 11:33:00 +0700

    slicing sign up - input part 1

--- lib/ui/pages/sign_up_page.dart
+++ lib/ui/pages/sign_up_page.dart
@@ -11,23 +11,68 @@ class SignUpPage extends StatelessWidget {
         margin: EdgeInsets.only(top: 30),
         child: Text(
           'Join us and get\nyour next journey',
-          style: blackStyle.copyWith(
-            fontSize: 24,
-            fontWeight: semibold
-          ),
+          style: blackStyle.copyWith(fontSize: 24, fontWeight: semibold),
         ),
+      );
+    }
+
+    Widget inputSection() {
+      Widget fieldInput() {
+        return Container(
+          margin: EdgeInsets.only(bottom: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Full Name',
+                style: blackStyle.copyWith(
+                  fontSize: 14,
+                  fontWeight: regular
+                ),
+              ),
+              SizedBox(
+                height: 6,
+              ),
+              TextFormField(
+                cursorColor: kBlackColor,
+                decoration: InputDecoration(
+                  hintText: 'Your fullname',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(
+                      defaultRadius
+                    ),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(
+                      defaultRadius
+                    ),
+                    borderSide: BorderSide(
+                      color: kPrimaryColor
+                    )
+                  )
+                ),
+              )
+            ],
+          ),
+        );
+      }
+
+      return Container(
+        margin: EdgeInsets.only(top: 30),
+        padding: EdgeInsets.symmetric(vertical: 30, horizontal: 20),
+        decoration: BoxDecoration(
+            color: kWhiteColor,
+            borderRadius: BorderRadius.circular(defaultRadius)
+        ),
+        child: fieldInput(),
       );
     }
 
     return Scaffold(
       body: SafeArea(
         child: ListView(
-          padding: EdgeInsets.symmetric(
-            horizontal: defaultMargin
-          ),
-          children: [
-            title()
-          ],
+          padding: EdgeInsets.symmetric(horizontal: defaultMargin),
+          children: [title(), inputSection()],
         ),
       ),
     );
