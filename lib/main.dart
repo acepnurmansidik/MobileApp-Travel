4e08edfb5534f0fb24cbfde053e2f6ed205561dc
acepnurmansidik <acepnurmansidik@gmail.com>
2022-04-16 09:45:54 +0700

    slicing get started

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'ui/pages/splash_page.dart';
+import 'package:airplane/ui/pages/get_started_page.dart';
 
 void main() => runApp(MyApp());
 
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SplashPage(),
+      home: GetStartedPage()
     );
   }
 }
---
+++ lib/ui/pages/get_started_page.dart
@@ -0,0 +1,65 @@
+import 'package:airplane/shared/theme.dart';
+import 'package:flutter/material.dart';
+
+class GetStartedPage extends StatelessWidget {
+  const GetStartedPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            width: double.infinity,
+            height: double.infinity,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/img_plane.png')
+              )
+            ),
+          ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Text('Fly like a bird', style: whiteStyle.copyWith(
+                  fontSize: 32,
+                  fontWeight: semibold
+                ),),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  'Explore new world with us and let\nyourself get an amazing experiences',
+                  style: whiteStyle.copyWith(
+                    fontSize: 16,
+                    fontWeight: light
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                Container(
+                  width: 220,
+                  height: 55,
+                  margin: EdgeInsets.only(top: 50, bottom: 80),
+                  child: TextButton(
+                    onPressed: (){},
+                    style: TextButton.styleFrom(
+                      backgroundColor: kPrimaryColor,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(17)
+                      )
+                    ),
+                    child: Text('Get Started',style: whiteStyle.copyWith(
+                      fontSize: 18,
+                      fontWeight: medium
+                    ),),
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
