834904b9ad2363abfc841f0928fae466a6fce597
acepnurmansidik <acepnurmansidik@gmail.com>
2022-04-17 11:16:32 +0700

    slicing home - popular destination

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -2,57 +2,147 @@ import 'package:flutter/material.dart';
 import '../../shared/theme.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({ Key? key }) : super(key: key);
+  const HomePage({Key? key}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return ListView(
-      children: [
-        Container(
-          margin: EdgeInsets.only(
-            left: defaultMargin,
-            right: defaultMargin,
-            top: 30,
+  Widget header() {
+    return Container(
+      margin: EdgeInsets.only(
+        left: defaultMargin,
+        right: defaultMargin,
+        top: 30,
+      ),
+      child: Row(
+        children: [
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Howdy,\nKezia Anne',
+                  style:
+                      blackStyle.copyWith(fontSize: 24, fontWeight: semibold),
+                  overflow: TextOverflow.ellipsis,
+                ),
+                SizedBox(
+                  height: 6,
+                ),
+                Text(
+                  'Where to fly today?',
+                  style: greyStyle.copyWith(fontSize: 16, fontWeight: light),
+                )
+              ],
+            ),
           ),
-          child: Row(
+          Container(
+            height: 60,
+            width: 60,
+            decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                image: DecorationImage(
+                    image: AssetImage('assets/images/img_profile.png'))),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget popularDestination() {
+    return Container(
+      margin: EdgeInsets.only(top: 30),
+      child: Row(
+        children: [
+          Container(
+          height: 323,
+          width: 200,
+          padding: EdgeInsets.all(10),
+          margin: EdgeInsets.only(left: 24),
+          decoration: BoxDecoration(
+            color: kWhiteColor,
+            borderRadius: BorderRadius.circular(18)
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Expanded(
+              Container(
+                height: 220,
+                width: 180,
+                margin: EdgeInsets.only(bottom: 20),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(18),
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/dest_1.png')
+                  )
+                ),
+                child: Align(
+                  alignment: Alignment.topRight,
+                  child: Container(
+                    width: 55,
+                    height: 30,
+                    decoration: BoxDecoration(
+                      color: kWhiteColor,
+                      borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(18)
+                      )
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Container(
+                          height: 18,
+                          width: 18,
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                              image: AssetImage('assets/icons/icon_star.png')
+                            )
+                          )
+                        ),
+                        SizedBox(width: 2,),
+                        Text(
+                          '4.8',
+                          style: greyStyle.copyWith(
+                            fontWeight: medium
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(left: 10),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Text(
-                      'Howdy,\nKezia Anne',
+                      'Lake Ciliwung',
                       style: blackStyle.copyWith(
-                        fontSize: 24,
-                        fontWeight: semibold
+                        fontSize: 18,
+                        fontWeight: medium
                       ),
-                      overflow: TextOverflow.ellipsis,
                     ),
-                    SizedBox(height: 6,),
+                    SizedBox(height: 5,),
                     Text(
-                      'Where to fly today?',
+                      'Tangerang',
                       style: greyStyle.copyWith(
-                        fontSize: 16,
                         fontWeight: light
                       ),
                     )
                   ],
                 ),
-              ),
-              Container(
-                height: 60,
-                width: 60,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  image: DecorationImage(
-                    image: AssetImage('assets/images/img_profile.png')
-                  )
-                ),
               )
             ],
           ),
         )
-      ],
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: [header(), popularDestination()],
     );
   }
 }
