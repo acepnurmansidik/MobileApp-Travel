1f76c1478dcbea91c828af1ea8c38cabc3e9d683
acepnurmansidik <acepnurmansidik@gmail.com>
2022-04-19 09:22:05 +0700

    Detail page - title

--- lib/ui/pages/DetailPage.dart
+++ lib/ui/pages/DetailPage.dart
@@ -22,21 +22,91 @@ class DetailPage extends StatelessWidget {
         width: double.infinity,
         margin: EdgeInsets.only(top: 236),
         decoration: BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            colors: [
+            gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
               kWhiteColor.withOpacity(0),
               Colors.black.withOpacity(0.95)
-            ]
-          )
+            ])),
+      );
+    }
+
+    Widget content() {
+      return Container(
+        width: double.infinity,
+        margin: EdgeInsets.symmetric(
+          horizontal: defaultMargin
+        ),
+        child: Column(
+          children: [
+            Container(
+              height: 24,
+              width: 108,
+              margin: EdgeInsets.only(top: 30),
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/icons/emblem.png')
+                )
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 256),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Lake Ciliwung',
+                          style: whiteStyle.copyWith(
+                            fontSize: 24,
+                            fontWeight: semibold
+                          ),
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        Text(
+                          'Tangerang',
+                          style: whiteStyle.copyWith(
+                            fontSize: 16,
+                            fontWeight: light
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  Row(
+                    children: [
+                      Container(
+                        width: 24,
+                        height: 24,
+                        margin: EdgeInsets.only(right: 2),
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: AssetImage('assets/icons/icon_star.png')
+                          )
+                        ),
+                      ),
+                      Text(
+                        '4.5',
+                        style: whiteStyle.copyWith(
+                          fontWeight: medium
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            )
+          ],
         ),
       );
     }
 
     return Scaffold(
       body: Stack(
-        children: [backgroundImage(), customeShadow()],
+        children: [backgroundImage(), customeShadow(), content()],
       ),
     );
   }
