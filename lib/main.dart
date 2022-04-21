9adf4510dde25385904d699f13ebb5ca9ec55c7a
acepnurmansidik <acepnurmansidik@gmail.com>
2022-04-21 14:09:53 +0700

    menambahkan firebase android

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:airplane/cubit/page_cubit.dart';
 import 'package:airplane/ui/pages/main_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:airplane/ui/pages/bonus_page.dart';
 import 'package:airplane/ui/pages/sign_up_page.dart';
@@ -7,7 +8,13 @@ import 'package:airplane/ui/pages/splash_page.dart';
 import 'package:airplane/ui/pages/get_started_page.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({ Key? key }) : super(key: key);
